9e63338cf4dcaf0a11663c8fc7fd4165a64324dd
Jordan Yaker <jordan.e.yaker@gmail.com>
2015-02-23 21:42:05 -0500

    Added the initial project frame.

---
+++ Web.Test/Program.fs
@@ -0,0 +1,14 @@
+﻿open canopy
+open runner
+open System
+
+//start an instance of the firefox browser
+start firefox
+
+//run all tests
+run()
+
+printfn "press [enter] to exit"
+System.Console.ReadLine() |> ignore
+
+quit()
